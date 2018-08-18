acb72ba0c8b3b582e8a4ca8c7d148bd7348a1ac9
tbricca <tbricca@gmail.com>
2018-08-17 18:00:08 -0700

    added routes folder and made files for companies, cities and transit routes

---
+++ config/keys.js
@@ -0,0 +1,3 @@
+module.exports = {
+  mongoURI: "mongodb://tony:tony12@ds123822.mlab.com:23822/transit-routes"
+};
--- server.js
+++ server.js
@@ -1,6 +1,20 @@
 const express = require("express");
+const mongoose = require("mongoose");
+
+const cities = require("./routes/api/cities");
+const companies = require("./routes/api/companies");
+const transitRoutes = require("./routes/api/transitRoutes");
 
 const app = express();
+
+// DB Config
+const db = require("./config/keys").mongoURI;
+
+// Connect to MongoDB
+mongoose
+  .connect(db)
+  .then(() => console.log("MongoDB Connected"))
+  .catch(err => console.log(err));
 
 app.get("/", (req, res) => res.send("Hello"));
 
