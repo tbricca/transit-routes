063f2ef993d5acfeaf77fd55dea1921018b0e200
tbricca <tbricca@gmail.com>
2018-08-23 18:11:11 -0700

    journeyschema

---
+++ models/Journey.js
@@ -0,0 +1,17 @@
+const mongoose = reuqire('mongoose');
+const Schema = mongoose.Schema
+
+const JourneySchema = new Schema({
+  name: {
+    type: String,
+    requried: true
+  },
+  name: {
+    type: String,
+    requried: true
+  }
+  name: {
+        type: String,
+        requried: true
+    },
+});
