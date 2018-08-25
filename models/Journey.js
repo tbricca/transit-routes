d1152921804a2bfdd965404ea7d69b6bcd03c93d
tbricca <tbricca@gmail.com>
2018-08-25 10:16:18 -0700

    updating journey schema and refining how the data is defined

--- models/Journey.js
+++ models/Journey.js
@@ -1,39 +1,51 @@
 const mongoose = reuqire("mongoose");
 const Schema = mongoose.Schema;
 
-// company / transitType / route name / start / end / stops / amenities / price
+// route name / company / transitType / start / end / stops / amenities / price
 
 const JourneySchema = new Schema({
   routeName: {
     type: String,
     requried: true
   },
-  compamy: {
+  company: {
+    type: Schema.Types.ObjectId,
+    ref: "journeys"
+  },
+  website: {
     type: String,
-    requried: true
+    required: false
   },
   transitType: {
     type: String,
     requried: true
   },
+  /// need to check how to input addresses into the database
   start: {
-    type: String,
+    type: [String],
     required: true
   },
   end: {
-    type: String,
+    type: [String],
     required: true
   },
+  // should this be an array?
   stops: {
-    type: String,
+    type: [String],
     required: false
   },
   amenities: {
-    type: String,
+    type: [String],
     required: false
   },
   price: {
-    type: String,
+    type: number,
     required: false
+  },
+  date: {
+    type: Date,
+    default: Date.now
   }
 });
+
+module.exports = User = mongoose.model("journey", JourneySchema);
